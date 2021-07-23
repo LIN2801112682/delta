9498b5aaf3ba76ae30adbc8e84a68aa91b8e3342
virgilwjj <virgilwjj@MBP.local>
2021-07-23 10:45:15 +0800

    update

--- src/neu/index_manager.cpp
+++ src/neu/index_manager.cpp
@@ -65,11 +65,50 @@ namespace neu
     index_manager::add_delta(const doc_id_type doc_id, const delta_type &delta)
     {
         delta_umap_.emplace(doc_id, delta);
+        auto merged_str{merge_str_by_delta(basic_str_, delta)};
+
+        offset_type begin{0}, end{0};
+        bool is_find_begin{false};
+        for (size_t i{0}; i < merged_str.size(); ++i)
+        {
+            const auto &ch{merged_str[i]};
+            if (!isSeparator(ch))
+            {
+                if (!is_find_begin)
+                {
+                    is_find_begin = true;
+                    begin = i;
+                }
+                end = i;
+            }
+            if (isSeparator(ch) || i == merged_str.size() - 1 && is_find_begin)
+            {
+                is_find_begin = false;
+                const auto &token = merged_str.substr(begin, end - begin + 1);
+
+                auto &doc_id_umap{inverted_index_[token]};
+                auto &offset_set{doc_id_umap[doc_id]};
+                offset_set.emplace(begin);
+            }
+        }
     }
 
     std::unordered_map<doc_id_type, offset_uset_type>
     index_manager::regex_query(const std::string &regex_str)
     {
-        return {};
+        std::unordered_map<doc_id_type, offset_uset_type> result;
+        std::regex pattern{regex_str};
+        std::smatch match{};
+        for (const auto &[token, doc_id_umap] : inverted_index_)
+        {
+            if (regex_match(token, match, pattern))
+            {
+                for (const auto &[doc_id, offset_set] : doc_id_umap)
+                {
+                    result[doc_id] = offset_set;
+                }
+            }
+        }
+        return result;
     }
 };
