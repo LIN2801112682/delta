cfb6ba950fd4741949ccd31f6ee7044cd2735062
virgilwjj <virgilwjj@gmail.com>
2021-07-30 20:42:16 +0800

    update

--- include/neu/core.h
+++ include/neu/core.h
@@ -8,6 +8,7 @@ namespace neu
     delta_t extract_delta(str_v_t basic_str_v, str_v_t native_str_v);
     str_t merge_str(str_v_t basic_str_v, const delta_t &delta);
     std::tuple<str_t, offset_t> partial_merge_str(str_v_t basic_str_v, const delta_t &delta, const size_t delta_idx, const check_dlm_func_t &check_dlm_func);
+    void batch_cal_native_offset(str_v_t basic_str_v, const delta_t &delta, std::vector<token_offset_t> &token_offset_vec, const check_dlm_func_t &check_dlm_func);
 };
 
 #endif
--- src/neu/core.cpp
+++ src/neu/core.cpp
@@ -446,4 +446,243 @@ namespace neu
         return {partial_merged_str, native_left_right_offset + 1};
     }
 
+    void batch_cal_native_offset(str_v_t basic_str_v, const delta_t &delta, std::vector<token_offset_t> &token_offset_vec, const check_dlm_func_t &check_dlm_func)
+    {
+        offset_t native_left_offset{0};
+        offset_t basic_left_offset{0};
+        offset_t basic_right_offset{static_cast<offset_t>(basic_str_v.size()) - 1};
+        int token_offset_vec_idx{0};
+        int mid{0};
+        while (mid < delta.size() && token_offset_vec_idx < token_offset_vec.size())
+        {
+            const auto &node{delta[mid]};
+            switch (node.type_)
+            {
+            case node_type_enum::insert:
+                basic_right_offset = node.low_;
+                break;
+            case node_type_enum::deletE:
+                basic_right_offset = node.low_ - 1;
+                break;
+            case node_type_enum::replace:
+                basic_right_offset = node.low_ - 1;
+                break;
+            default:
+                std::cerr << "error node type enum: " << static_cast<int>(node.type_) << '\n';
+                break;
+            }
+
+            if (basic_left_offset <= basic_right_offset)
+            {
+                for (; token_offset_vec_idx < token_offset_vec.size(); ++token_offset_vec_idx)
+                {
+                    const auto &token_offset{token_offset_vec[token_offset_vec_idx]};
+                    if (basic_right_offset < token_offset.offset)
+                    {
+                        break;
+                    }
+
+                    bool has_left_split{false};
+                    if (basic_left_offset < token_offset.offset)
+                    {
+                        has_left_split = true;
+                    }
+                    else if (basic_left_offset == token_offset.offset)
+                    {
+                        auto left{mid - 1};
+                        if (left < 0)
+                        {
+                            has_left_split = true;
+                        }
+                        else
+                        {
+                            const auto &left_node{delta[left]};
+                            offset_t pre_offset{-1}; // case init
+                            switch (left_node.type_)
+                            {
+                            case node_type_enum::insert:
+                                if (check_dlm_func(left_node.content_[left_node.content_.size() - 1]))
+                                {
+                                    has_left_split = true;
+                                }
+                                break;
+                            case node_type_enum::deletE:
+                                // ignore continuial detla situation, todo
+                                pre_offset = left_node.low_ - 1;
+                                if (pre_offset < 0)
+                                {
+                                    has_left_split = true;
+                                }
+                                if (0 <= pre_offset && check_dlm_func(basic_str_v[pre_offset]))
+                                {
+                                    has_left_split = true;
+                                }
+                                break;
+                            case node_type_enum::replace:
+                                if (check_dlm_func(left_node.content_[left_node.content_.size() - 1]))
+                                {
+                                    has_left_split = true;
+                                }
+                                break;
+                            default:
+                                std::cerr << "error node type enum: " << static_cast<int>(left_node.type_) << '\n';
+                                break;
+                            }
+                        }
+                    }
+
+                    if (has_left_split)
+                    {
+                        bool has_right_split{false};
+                        if (token_offset.offset + token_offset.token.size() - 1 < basic_right_offset)
+                        {
+                            has_right_split = true;
+                        }
+                        else if (token_offset.offset + token_offset.token.size() - 1 == basic_right_offset)
+                        {
+                            auto right{mid + 1};
+                            if (delta.size() - 1 < right)
+                            {
+                                has_right_split = true;
+                            }
+                            else
+                            {
+                                const auto &right_node{delta[right]};
+                                offset_t post_offset{static_cast<offset_t>(basic_str_v.size())}; // case init
+                                switch (right_node.type_)
+                                {
+                                case node_type_enum::insert:
+                                    if (check_dlm_func(right_node.content_[0]))
+                                    {
+                                        has_right_split = true;
+                                    }
+                                    break;
+                                case node_type_enum::deletE:
+                                    // ignore continuial detla situation, todo
+                                    post_offset = right_node.high_ + 1;
+                                    if (basic_str_v.size() - 1 < post_offset)
+                                    {
+                                        has_right_split = true;
+                                    }
+                                    if (post_offset <= basic_str_v.size() - 1 && check_dlm_func(basic_str_v[post_offset]))
+                                    {
+                                        has_right_split = true;
+                                    }
+                                    break;
+                                case node_type_enum::replace:
+                                    if (check_dlm_func(right_node.content_[0]))
+                                    {
+                                        has_right_split = true;
+                                    }
+                                    break;
+                                default:
+                                    std::cerr << "error node type enum: " << static_cast<int>(right_node.type_) << '\n';
+                                    break;
+                                }
+                            }
+                        }
+                        if (has_right_split)
+                        {
+                            result[doc_id].emplace(token_offset.offset + native_left_offset - basic_left_offset);
+                        }
+                    }
+                }
+            }
+
+            switch (node.type_)
+            {
+            case node_type_enum::insert:
+                native_left_offset = node.native_right_left_offset_ + node.content_.size() + 1;
+                basic_left_offset = node.high_;
+                break;
+            case node_type_enum::deletE:
+                native_left_offset = node.native_right_left_offset_ + 1;
+                basic_left_offset = node.high_ + 1;
+                break;
+            case node_type_enum::replace:
+                native_left_offset = node.native_right_left_offset_ + node.content_.size() + 1;
+                basic_left_offset = node.high_ + 1;
+                break;
+            default:
+                std::cerr << "error node type enum: " << static_cast<int>(node.type_) << '\n';
+                break;
+            }
+            ++mid;
+        }
+
+        if (token_offset_vec_idx < token_offset_vec.size())
+        {
+            basic_right_offset = static_cast<offset_t>(basic_str_v.size()) - 1;
+            if (basic_left_offset <= basic_right_offset)
+            {
+                for (; token_offset_vec_idx < token_offset_vec.size(); ++token_offset_vec_idx)
+                {
+                    const auto &token_offset{token_offset_vec[token_offset_vec_idx]};
+                    if (basic_right_offset < token_offset.offset)
+                    {
+                        break;
+                    }
+
+                    bool has_left_split{false};
+                    if (basic_left_offset < token_offset.offset)
+                    {
+                        has_left_split = true;
+                    }
+                    else if (basic_left_offset == token_offset.offset)
+                    {
+                        auto left{mid - 1};
+                        if (left < 0)
+                        {
+                            has_left_split = true;
+                        }
+                        else
+                        {
+                            const auto &left_node{delta[left]};
+                            offset_t pre_offset{-1}; // case init
+                            switch (left_node.type_)
+                            {
+                            case node_type_enum::insert:
+                                if (check_dlm_func(left_node.content_[left_node.content_.size() - 1]))
+                                {
+                                    has_left_split = true;
+                                }
+                                break;
+                            case node_type_enum::deletE:
+                                // ignore continuial detla situation, todo
+                                pre_offset = left_node.low_ - 1;
+                                if (0 <= pre_offset && check_dlm_func(basic_str_v[pre_offset]))
+                                {
+                                    has_left_split = true;
+                                }
+                                break;
+                            case node_type_enum::replace:
+                                if (check_dlm_func(left_node.content_[left_node.content_.size() - 1]))
+                                {
+                                    has_left_split = true;
+                                }
+                                break;
+                            default:
+                                std::cerr << "error node type enum: " << static_cast<int>(left_node.type_) << '\n';
+                                break;
+                            }
+                        }
+                    }
+
+                    if (has_left_split)
+                    {
+                        bool has_right_split{false};
+                        if (token_offset.offset + token_offset.token.size() <= basic_right_offset)
+                        {
+                            has_right_split = true;
+                        }
+
+                        if (has_right_split)
+                        {
+                            result[doc_id].emplace(token_offset.offset + native_left_offset - basic_left_offset);
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
