235007bad12c956b5ba16b1abb2cb62370f9ca9e
virgilwjj <virgilwjj@gmail.com>
2021-12-16 21:00:09 +0800

    update

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -4,7 +4,6 @@
 
 static constexpr auto basic_file_path{"../resources/chr3.txt"};
 static constexpr auto native_file_path{"../resources/query2.txt"};
-static constexpr auto save_file_path{"../resources/save.txt"};
 
 int main()
 {
@@ -12,34 +11,12 @@ int main()
 
     auto native_str_vec{load_all_line(native_file_path)};
 
-    std::ofstream ofs{save_file_path, std::ios::out};
-    SCOPE_GUARD
-    {
-        ofs.close();
-    };
-
     for (const auto &native_str : native_str_vec)
     {
         auto delta{neu::extract_delta(basic_str, native_str)};
         auto save_str{neu::serialize(delta)};
-        ofs << save_str << '\n';
-    }
-    std::cout << "save suc.\n";
-
-    auto save_str_vec{load_all_line(save_file_path)};
-    std::cout << "load suc.\n";
-
-    if (native_str_vec.size() != save_str_vec.size())
-    {
-        std::cerr << "native_size: " << native_str_vec.size() << ' '
-                  << "save_size: " << save_str_vec.size() << '\n';
-    }
-
-    int i = 0;
-    for (const auto &native_str : native_str_vec)
-    {
-        auto delta{neu::deserialize(save_str_vec[i++])};
-        auto merged_str{neu::merge_str(basic_str, delta)};
+        auto deserialize_delta{neu::deserialize(save_str)};
+        auto merged_str{neu::merge_str(basic_str, deserialize_delta)};
         if (native_str != merged_str)
         {
             std::cerr << "basic_str: " << basic_str << '\n'
